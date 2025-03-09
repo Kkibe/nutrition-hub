18b1cfd70661d10310f97456d5217d77bb05e032
Kibet <137826199+Kkibe@users.noreply.github.com>
2025-03-10 01:35:34 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged, updateProfile } from "firebase/auth";
 import { ref } from "firebase/database";
 //import { collection, addDoc, ref, uploadBytes, getDoc, enableIndexedDbPersistence, getFirestore } from "firebase/firestore";
-import {addDoc, collection, deleteDoc, doc, enableIndexedDbPersistence, getDoc, getDocs, getFirestore, limit, orderBy, query, updateDoc } from "firebase/firestore";
+import {addDoc, collection, deleteDoc, doc,  getDoc, getDocs, getFirestore, limit, orderBy, query, updateDoc,  initializeFirestore, persistentLocalCache } from "firebase/firestore";
 
 
 const firebaseConfig = {
@@ -18,12 +18,13 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+//export const db = getFirestore(app);
+export const db = initializeFirestore(app, {
+  localCache: persistentLocalCache()
+});
 //const storage = getStorage(app);
 //const functionsInstance = functions(app);
 export const auth = getAuth(app);
-// Enable offline persistence
-await enableIndexedDbPersistence(db);
 //additional features
 const analytics = getAnalytics(app);
 //const remoteConfig = getRemoteConfig(app);
